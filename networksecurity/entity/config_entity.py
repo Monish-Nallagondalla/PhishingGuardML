5e6106cad07908f6e4282178c49fa03e818363e8
Monish Nallagondalla <nsmonish@gmail.com>
2025-06-07 13:38:57 +0530

    entity and data init

---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import os 
+from networksecurity.constant import training_pipeline
+
+print(training_pipeline.ARTIFACT_DIR)
+print(training_pipeline.PIPELINE_NAME)
+
+class TrainingPipelineConfig:
+    def __init__(self,timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name = training_pipeline.PIPELINE_NAME
+        self.artifact_name = training_pipeline.ARTIFACT_DIR
+        self.artifact_dir = os.path.join(self.artifact_dir,timestamp)
+        self.model_dir = os.path.join("final_model")
+        self.timestamp: str = timestamp
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir: str = 
