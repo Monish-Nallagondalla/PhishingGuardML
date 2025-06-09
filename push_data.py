60cfabe24fce435952011959e2bfdb02cb4e163c
Monish Nallagondalla <nsmonish@gmail.com>
2025-06-09 20:28:12 +0530

    in progress tsting validation , stuck with code errors

--- push_data.py
+++ push_data.py
@@ -50,7 +50,7 @@ class NetworkDataExtract():
         
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="cluster01"
+    DATABASE="cluster0"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
