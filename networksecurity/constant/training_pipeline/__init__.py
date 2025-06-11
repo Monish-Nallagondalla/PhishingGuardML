8dd651a4e69439bf5091a8e8e2fedbc0d468232d
Monish Nallagondalla <nsmonish@gmail.com>
2025-06-12 02:39:26 +0530

    s3 connected and artifact and final model uploaded to AWS s3

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,32 +1,30 @@
-import os 
+import os
 import sys
-
 import numpy as np
-import pandas as pd 
-
+import pandas as pd
 
 """
-defining common constant variables for training pipeline 
+defining common constant variable for training pipeline
 """
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
 
-TARGET_COLUMN = 'Result'
-PIPELINE_NAME : str = "NetworkSecurity"
-ARTIFACT_DIR : str = "Artifacts"
-FILE_NAME : str = "phisingData.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
 
-TRAIN_FILE_NAME :str = "train.csv"
-TEST_FILE_NAME : str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
-SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
-SAVED_MODEL_DIR= os.path.join("saved_models")
-MODEL_FILE_NAME = 'model.pkl'
+
 
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
-
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "cluster0"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
@@ -44,25 +42,22 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
-
 """
 Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 """
-DATA_TRANSFORMATION_DIR_NAME: str= "data_transformation"
-DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str =  "transformed"
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
 ## kkn imputer to replace nan values
-DATA_TRANSFORMATION_IMPUTER_PARAMS: dict =  {
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
     "n_neighbors": 3,
     "weights": "uniform",
 }
-DATA_TRANSFORMATION_TRAIN_FILE_PATH:str = "train.npy"
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 
-DATA_TRANSFORMATION_TEST_FILE_PATH: str  = "test.npy"
-
-
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 
 """
@@ -75,4 +70,4 @@ MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
-TRAINING_BUCKET_NAME = "netwworksecurity"
+TRAINING_BUCKET_NAME = "phishingguardml"
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -9,10 +9,6 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
 
-
-
-from networksecurity.components.model_trainer import ModelTrainer
-
 from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
@@ -21,8 +17,6 @@ from networksecurity.entity.config_entity import(
     ModelTrainerConfig,
 )
 
-
-
 from networksecurity.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
@@ -30,12 +24,17 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
+from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
+import sys
+
 
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config=TrainingPipelineConfig()
-
-    
+        self.s3_sync = S3Sync()
+        
 
     def start_data_ingestion(self):
         try:
@@ -49,8 +48,6 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-
-            
     def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
             data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
@@ -60,8 +57,6 @@ class TrainingPipeline:
             return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
-
         
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
@@ -73,8 +68,7 @@ class TrainingPipeline:
             return data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-
-
+        
     def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
         try:
             self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(
@@ -92,7 +86,26 @@ class TrainingPipeline:
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+    ## local artifact is going to s3 bucket    
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
         
+    ## local final model is going to s3 bucket 
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    
+    
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
@@ -100,6 +113,8 @@ class TrainingPipeline:
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             
             return model_trainer_artifact
         except Exception as e:
